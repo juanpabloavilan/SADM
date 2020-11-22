a05e1ecfe50704630a39392bc21a6213da757468
Juan Pablo Avilan Moreno <avilanjuanpablo@gmail.com>
2020-11-21 20:00:06 -0500

    Multa, MultaPP, Propietario, Soat, Vehiculo, BST Added

---
+++ src/mundo/Multa.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mundo;
+import util.EstampaTiempo;
+
+/**
+ *
+ * @author AVILAN MORENO
+ */
+public class Multa {
+   protected String codigo;
+    protected String tipo;
+    protected double valor;
+    protected EstampaTiempo fecha;
+
+    public Multa(String codigo, String tipo, EstampaTiempo fecha) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.tipo = tipo;
+        
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public EstampaTiempo getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(EstampaTiempo fecha) {
+        this.fecha = fecha;
+    } 
+}
---
+++ src/mundo/Propietario.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mundo;
+
+/**
+ *
+ * @author AVILAN MORENO
+ */
+public class Propietario {
+    final private long cedula;
+    final private String nombres;
+    final private String apellidos;
+    final private String direccion;
+    final private long telefono;
+    final private String correo;
+    
+    public Propietario(long cedula, String nombres, String apellidos, String direccion, long telefono, String correo){
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cedula:" + cedula + ", Nombres:" + nombres + ", Apellidos:" + apellidos + ", Direccion:" + direccion + ", Telefono:" + telefono + ", Correo:" + correo;
+    }
+}
